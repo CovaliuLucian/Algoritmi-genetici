1afd07629902f69a8360bbf47408f0c80e78ae7c
Covaliu Lucian <luciancovaliu41@gmail.com>
2017-12-08 11:05:06 +0200

    Tema 2 Prim
    To optimize

---
+++ Tema 2 Prim/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+using Functions;
+using Tema_2;
+using Tema_2.Selections;
+using Tema_2_Prim.Functions;
+
+namespace Tema_2_Prim
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            var rez = Minimum.GetMinimum(new FunctieTema2Prim(), new Tourney(), 2, 100);
+            Console.WriteLine(stopwatch.Elapsed);
+            Console.WriteLine(Math.Round(rez,4));
+        }
+    }
+}
--- Tema 2/Minimum.cs
+++ Tema 2/Minimum.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Diagnostics;
-using System.IO;
 using System.Linq;
 using Functions;
 using Tema_2.Selections;
@@ -24,52 +23,38 @@ namespace Tema_2
         public static double GetMinimum(IFunction function, ISelection selector, int dimensions, int size,
             double crossChance = 0.5, double mutationChance = 0.3)
         {
-            using (var writer = File.AppendText("Counting.txt"))
+            function.Counter = 0;
+            Stopwatch.Restart();
+            var population = Generator.GeneratePopulation(dimensions, function, size);
+            const int maxIterations = 10;
+            var count = 0;
+            var timesForBreak = 5;
+            var lastMinimum = population.MinimOfPopulation(function);
+            //while (population.HammingDistance() > 2 && count != maxIterations)
+            while (count != maxIterations)
             {
-                writer.AutoFlush = true;
-                writer.WriteLine();
-                function.Counter = 0;
-                Stopwatch.Restart();
-                var population = Generator.GeneratePopulation(dimensions, function, size);
-                const int maxIterations = 1000;
-                var count = 0;
-                var timesForBreak = 5;
-                var totalCounter = 0;
-                var lastMinimum = population.MinimOfPopulation(function);
-                //while (population.HammingDistance() > 2 && count != maxIterations)
-                while (count != maxIterations)
+                count++;
+                if (count % 10 == 0)
                 {
-                    count++;
-                    if (count % 10 == 0)
-                    {
-                        var currentMinimum = population.MinimOfPopulation(function);
-                        if (Math.Abs(currentMinimum - lastMinimum) < 0.00001)
-                            if (timesForBreak == 0)
-                                break;
-                            else
-                                timesForBreak--;
+                    var currentMinimum = population.MinimOfPopulation(function);
+                    if (Math.Abs(currentMinimum - lastMinimum) < 0.00001)
+                        if (timesForBreak == 0)
+                            break;
                         else
-                            lastMinimum = currentMinimum;
-
-
-                        if (function.Counter > 1000 || totalCounter == 0)
-                        {
-                            totalCounter += function.Counter;
-                            writer.WriteLine(function.GetName() + " " + totalCounter + " at " +
-                                             Math.Round(population.MinimOfPopulation(function), 4));
-                            function.Counter = 0;
-                        }
-                    }
-
-                    population = selector.Select(population, function);
-                    population = population.Mutate(function, mutationChance);
-                    population = population.CrossOver(function, crossChance);
+                            timesForBreak--;
+                    else
+                        lastMinimum = currentMinimum;
                 }
-                //population.Write();
-                //Console.WriteLine(population.HammingDistance() + "\n");
-                //Console.WriteLine(population.StandardDeviation(function) + "\n");
-                return population.MinimOfPopulation(function);
+
+                population = selector.Select(population, function);
+                population = population.Mutate(function, mutationChance);
+                population = population.CrossOver(function, crossChance);
             }
+            //population.Write();
+            //Console.WriteLine(population.HammingDistance() + "\n");
+            //Console.WriteLine(population.StandardDeviation(function) + "\n");
+            
+            return population.MinimOfPopulation(function);
         }
     }
 }
